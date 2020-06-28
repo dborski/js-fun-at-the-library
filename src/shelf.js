bd05fdff8eab64edc984a7f36e447f4b72a87e4d
Derek Borski <derek.borski@gmail.com>
2020-06-28 00:17:17 -0600

    Complete shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,28 @@
+function shelfBook(book, shelf) {
+  if (shelf.length >= 3) {
+    return shelf
+  } else {
+    return shelf.unshift(book)
+  }
+}
 
+function unshelfBook(bookSeeking, shelf) {
+  var bookIndex = shelf.findIndex(book => book.title == bookSeeking)
+  shelf.splice(bookIndex, 1)
+}
+
+function listTitles(shelf) {
+  var allTitles = shelf.flatMap(book => book.title)
+  return allTitles.join(", ")
+}
+
+function searchShelf(shelf, bookTitle) {
+  return listTitles(shelf).includes(bookTitle)
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
